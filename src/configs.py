7188ed7363582973dea21840fc21635bb2ec665b
maksym.palamariuk <maksym.palamariuk@cleverbridge.com>
2024-11-19 17:00:46 +0200

    .

--- src/configs.py
+++ src/configs.py
@@ -7,11 +7,12 @@ SAMPLE_RATE = 0.2  # Keep 20% of records
 
 # Output Stream
 BROKER_TYPE = 'Socket'  # or "Kafka"
+# BROKER_TYPE = 'Kafka'  # or "Kafka"
 
 # Socket Configuration (legacy)
 SOCKET_HOST = 'localhost'
 SOCKET_PORT = 9999
 
 # Kafka settings
-KAFKA_BROKER = "localhost:9092"  # Kafka broker address (use "kafka:9092" in Docker if services are linked)
+KAFKA_BROKER = "kafka:9092"  # Kafka broker address
 KAFKA_TOPIC = "wiki-changes"  # Topic to which the data will be published
